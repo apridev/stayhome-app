217c491cc936ac1876e5be7a79080dad1ac421ac
Apridev <apriantofajar11@gmail.com>
2022-08-26 17:33:58 +0900

    template app

---
+++ lib/template.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color primaryColor = Color(0xff3792D6);
+Color blackColor = Color(0xff132C33);
+Color subBlackColor = Color(0xffA5ADAF);
+Color verificationTextColor = Color(0xff5E6F74);
+Color starColor = Color(0xffFFB200);
+Color subPrimaryColor = Color(0xffEBF4FA);
+
+TextStyle primaryTextStyle = GoogleFonts.openSans(
+  color: primaryColor
+);
+
+TextStyle blackTextStyle = GoogleFonts.openSans(
+  color: blackColor
+);
+
+TextStyle subBlackTextStyle = GoogleFonts.openSans(
+  color: subBlackColor
+);
+
+TextStyle verificationTextStyle = GoogleFonts.openSans(
+  color: verificationTextColor
+);
+
+TextStyle subPrimaryTextStyle = GoogleFonts.openSans(
+  color: subPrimaryColor
+);
+
+FontWeight reguler = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
