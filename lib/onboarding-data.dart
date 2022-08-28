c06bd0ae925f9771b395b23128c217e2bb69a763
Apridev <apriantofajar11@gmail.com>
2022-08-28 23:11:13 +0900

    onboarding data

---
+++ lib/onboarding-data.dart
@@ -0,0 +1,24 @@
+class OnBoarding {
+  final String title;
+  final String image;
+
+  OnBoarding({
+    required this.title,
+    required this.image,
+  });
+}
+
+List <OnBoarding> onBoardingContent = [
+  OnBoarding(
+    title: 'Find your dream home only here',
+    image: 'assets/imgs/onboarding-1.png'
+  ),
+  OnBoarding(
+    title: 'Find your dream home only here',
+    image: 'assets/imgs/onboarding-2.png'
+  ),
+  OnBoarding(
+    title: 'Find your dream home only here',
+    image: 'assets/imgs/onboarding-3.png'
+  ),
+];
