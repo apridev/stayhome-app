ce7e82c4e5284065f8ea0771376fdacd152042cc
Apridev <apriantofajar11@gmail.com>
2022-08-29 12:11:21 +0900

    main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:homestay_app/onboarding-page.dart';
+import 'package:homestay_app/page/login-page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: OnBoardingPage(),
+      routes: {
+        '/' :(context) => OnBoardingPage(),
+        '/login-page' :(context) => LoginPage(),
+      },
     );
   }
 }
