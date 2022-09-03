24dffd0ae976ac379764dcc4f5b35756a9f1d0ff
Apridev <apriantofajar11@gmail.com>
2022-09-03 22:08:57 +0900

    chat page

---
+++ lib/home/chat-page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:homestay_app/template.dart';
+
+class ChatPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: whiteColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text('Chat Page'),
+          ),
+        ],
+      ),
+    );
+  }
+}
