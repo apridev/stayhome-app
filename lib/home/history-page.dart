0ca1e75447697ea0a55e316485d832e3c7ad58c7
Apridev <apriantofajar11@gmail.com>
2022-09-03 22:09:09 +0900

    history page

---
+++ lib/home/history-page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:homestay_app/template.dart';
+
+class HistoryPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: whiteColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text('History Page'),
+          )
+        ],
+      ),
+    );
+  }
+}
