35c723e87288743628b58ffcb6bc36a7d5ea42c6
Apridev <apriantofajar11@gmail.com>
2022-09-06 23:35:19 +0900

    popular page

--- lib/widgets/popular-page-home.dart
+++ lib/widgets/popular-page-home.dart
@@ -23,145 +23,150 @@ class PopularPageHomeTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(
-        left: 20
-      ),
-      height: 255,
-      width: 230,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(24),
-          color: whiteColor,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.2),
-              spreadRadius: 2,
-              blurRadius: 3,
-              offset: Offset(0, 1), // changes position of shadow
+    return GestureDetector(
+      onTap: (){
+        Navigator.pushNamed(context, '/detail-product-page');
+      },
+      child: Container(
+        margin: EdgeInsets.only(
+          left: 20
+        ),
+        height: 255,
+        width: 230,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(24),
+            color: whiteColor,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.2),
+                spreadRadius: 2,
+                blurRadius: 3,
+                offset: Offset(0, 1), // changes position of shadow
+              ),
+            ]),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 150,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(images),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(24),
+                      topRight: Radius.circular(24))),
             ),
-          ]),
-      child: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            height: 150,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(images),
-                  fit: BoxFit.cover,
-                ),
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(24),
-                    topRight: Radius.circular(24))),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 12, left: 10, right: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          nameLocation,
-                          style: blackTextStyle.copyWith(
-                              fontSize: 14, fontWeight: semiBold),
-                        ),
-                        SizedBox(
-                          height: 4,
-                        ),
-                        Text(
-                          location,
-                          style: subPrimaryTextStyle2.copyWith(fontSize: 12),
-                        )
-                      ],
-                    ),
-                    Expanded(
-                        child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                          '\$${price.toString()}',
-                          style: primaryTextStyle.copyWith(
-                              fontSize: 14, fontWeight: semiBold),
-                        ),
-                        Text(
-                          '/month',
-                          style: subPrimaryTextStyle2.copyWith(fontSize: 12),
-                        )
-                      ],
-                    ))
-                  ],
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      child: Row(
+            Container(
+              margin: EdgeInsets.only(top: 12, left: 10, right: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Image.asset(
-                            'assets/icons/icon-sofa.png',
-                            width: 35,
+                          Text(
+                            nameLocation,
+                            style: blackTextStyle.copyWith(
+                                fontSize: 14, fontWeight: semiBold),
                           ),
                           SizedBox(
-                            width: 5,
+                            height: 4,
                           ),
                           Text(
-                            sofa,
-                            style: blackTextStyle.copyWith(
-                                fontSize: 12, fontWeight: semiBold),
+                            location,
+                            style: subPrimaryTextStyle2.copyWith(fontSize: 12),
                           )
                         ],
                       ),
-                    ),
-                    Container(
-                      child: Row(
+                      Expanded(
+                          child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
                         children: [
-                          Image.asset(
-                            'assets/icons/icon-bathroom.png',
-                            width: 35,
-                          ),
-                          SizedBox(
-                            width: 5,
+                          Text(
+                            '\$${price.toString()}',
+                            style: primaryTextStyle.copyWith(
+                                fontSize: 14, fontWeight: semiBold),
                           ),
                           Text(
-                            bathroom,
-                            style: blackTextStyle.copyWith(
-                                fontSize: 12, fontWeight: semiBold),
+                            '/month',
+                            style: subPrimaryTextStyle2.copyWith(fontSize: 12),
                           )
                         ],
+                      ))
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        child: Row(
+                          children: [
+                            Image.asset(
+                              'assets/icons/icon-sofa.png',
+                              width: 35,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              sofa,
+                              style: blackTextStyle.copyWith(
+                                  fontSize: 12, fontWeight: semiBold),
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                    Container(
-                      child: Row(
-                        children: [
-                          Image.asset(
-                            'assets/icons/icon-home.png',
-                            width: 35,
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            home,
-                            style: blackTextStyle.copyWith(
-                                fontSize: 12, fontWeight: semiBold),
-                          )
-                        ],
+                      Container(
+                        child: Row(
+                          children: [
+                            Image.asset(
+                              'assets/icons/icon-bathroom.png',
+                              width: 35,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              bathroom,
+                              style: blackTextStyle.copyWith(
+                                  fontSize: 12, fontWeight: semiBold),
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                      Container(
+                        child: Row(
+                          children: [
+                            Image.asset(
+                              'assets/icons/icon-home.png',
+                              width: 35,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              home,
+                              style: blackTextStyle.copyWith(
+                                  fontSize: 12, fontWeight: semiBold),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
